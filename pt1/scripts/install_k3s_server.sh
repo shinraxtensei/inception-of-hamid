73248c7dd64d71125790df0e7eba45027cf1b2b1
anasshouarii@gmail.com <anasshouarii@gmail.com>
2025-10-08 10:56:58 +0100

    Update K3s installation script to wait for readiness and adjust Vagrant server resources

--- pt1/scripts/install_k3s_server.sh
+++ pt1/scripts/install_k3s_server.sh
@@ -10,13 +10,10 @@ curl -sfL https://get.k3s.io | sh -s - \
   --bind-address=192.168.56.110 \
   --node-ip=192.168.56.110
 
-# Wait for K3s to be ready
-until sudo kubectl get nodes 2>/dev/null; do
-  echo "Waiting for K3s to start..."
-  sleep 5
-done
-
 # Save token for worker node
 sudo cat /var/lib/rancher/k3s/server/node-token > /vagrant/node-token
+
+# Add alias for kubectl as 'k'
+echo "alias k='kubectl'" >> ~/.bashrc
 
 echo "K3s server installed successfully!"
